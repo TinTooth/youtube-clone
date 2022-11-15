42f95509f4f589ee1b4acca8375422c2db73babd
TinTooth <boconne4@gmail.com>
2022-11-15 14:25:17 -0600

    Added ERD and postmanfiles

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -4,9 +4,8 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id','user','video_id','text','likes','dislikes','user_id' ]
+        fields = ['id','user','video_id','text','likes','dislikes' ]
         depth = 1
 
-    user_id = serializers.IntegerField(write_only = True)
 
     
