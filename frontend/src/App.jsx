4a4094cec778e7a0bddf472e8cdaaa0272fd31a3
TinTooth <boconne4@gmail.com>
2022-11-16 14:12:38 -0600

    Removed Footer component + Added SearchPage + Updated App to default to SearchPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Pages Imports
-import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-
+import SearchPage from "./pages/SearchPage/SearchPage";
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
@@ -19,18 +17,10 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/" element={<SearchPage />}/>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
---
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+const SearchPage = () => {
+    return (  null);
+}
+ 
+export default SearchPage;
